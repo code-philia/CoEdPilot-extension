d183b96c59daa41f82b3141c46201953d43d274b
trick-treat <1076689101@qq.com>
2023-11-19 20:44:21 +0800

    Add time metric in model server

--- src/model_server/server.py
+++ src/model_server/server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, make_response
 from discriminator.interface import predict as disc_predict
 from locator.interface import predict as loc_predict
 from generator.interface import predict as gen_predict
+import time
 import json
 
 app = Flask(__name__)
@@ -13,15 +14,21 @@ def make_plain_text_response(result):
     return response
 
 @app.route('/discriminator', methods=['POST'])
+
 def run_discriminator():
     print(">>> Running discriminator")
     json_str = request.data.decode('utf-8')
     input_json = json.loads(json_str)
 
-    print(f">>> Discriminator inferencing: \n${json.dumps(input_json, indent=4)}")
+    print(f">>> Discriminator infering: \n${json.dumps(input_json, indent=4)}")
+    start_time = time.time()
     result = disc_predict(input_json)
-
+    
+    end_time = time.time()
+    inference_time = end_time - start_time
+    
     print(f">>> Discriminator sending output: \n${json.dumps(result, indent=4)}")
+    print(f"Inference time: {inference_time:.3f} seconds")   
     return make_plain_text_response(result)
 
 @app.route('/range', methods=['POST'])
@@ -30,10 +37,15 @@ def run_range():
     json_str = request.data.decode('utf-8')
     input_json = json.loads(json_str)
     
-    print(f">>> Locator inferencing: \n${json.dumps(input_json, indent=4)}")
+    print(f">>> Locator infering: \n${json.dumps(input_json, indent=4)}")
+    start_time = time.time()
     result = loc_predict(input_json)
 
+    end_time = time.time()
+    inference_time = end_time - start_time
+
     print(f">>> Locator sending output: \n${json.dumps(result, indent=4)}")
+    print(f"Inference time: {inference_time:.3f} seconds")
     return make_plain_text_response(result)
 
 @app.route('/content', methods=['POST'])
@@ -42,11 +54,16 @@ def run_content():
     json_str = request.data.decode('utf-8')
     input_json = json.loads(json_str)
 
-    print(f">>> Editor inferencing: \n${json.dumps(input_json, indent=4)}")
+    print(f">>> Editor infering: \n${json.dumps(input_json, indent=4)}")
+    start_time = time.time()
     result = gen_predict(input_json)
-    
+
+    end_time = time.time()
+    inference_time = end_time - start_time
+
     print(f">>> Editor sending output: \n${json.dumps(result, indent=4)}")
+    print(f"Inference time: {inference_time:.3f} seconds")
     return make_plain_text_response(result)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5004, debug=True)
+    app.run(host='localhost', port=5012, debug=True)
