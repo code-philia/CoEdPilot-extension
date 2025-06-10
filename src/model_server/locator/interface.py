6df74b220fdb85a2679fe1b05f1e9ed26ddf9793
chenyan1999 <chenyanliu712@gmail.com>
2025-06-10 16:30:05 +0800

    fix: when a single line of code is too long

--- src/model_server/locator/interface.py
+++ src/model_server/locator/interface.py
@@ -293,8 +293,9 @@ async def predict(json_input):
             else:
                 if window_line_cnt == 0:    # the first line is longer than window limit
                     while True:
-                        cur_line = cur_line[:len(cur_line) / 2]
+                        cur_line = cur_line[:len(cur_line) // 2]
                         if try_feed_in_window(cur_line):
+                            i += 1
                             break
                 else:
                     end_window(model_inputs)
